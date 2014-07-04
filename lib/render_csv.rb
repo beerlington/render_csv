7869f75c80463dd49509419ab6329304f53d4a72
Thomas Ingram <code@ravinggenius.com>
2014-07-04 11:29:09 -0400

    Always call to_csv

--- lib/render_csv.rb
+++ lib/render_csv.rb
@@ -7,7 +7,7 @@ module RenderCsv
       ActionController.add_renderer :csv do |csv, options|
         filename = options[:filename] || options[:template]
         csv.extend RenderCsv::CsvRenderable
-        data = csv.respond_to?(:to_csv) ? csv.to_csv(options) : csv
+        data = csv.to_csv(options)
         send_data data, type: Mime::CSV, disposition: "attachment; filename=#{filename}.csv"
       end
     end
