18522ef9c20f642a8df6ea85667c38867f5592e6
Thomas Ingram <code@ravinggenius.com>
2014-07-04 11:31:15 -0400

    Don't override to_csv if object already defines it

--- lib/render_csv.rb
+++ lib/render_csv.rb
@@ -6,7 +6,7 @@ module RenderCsv
 
       ActionController.add_renderer :csv do |csv, options|
         filename = options[:filename] || options[:template]
-        csv.extend RenderCsv::CsvRenderable
+        csv.extend RenderCsv::CsvRenderable unless csv.respond_to?(:to_csv)
         data = csv.to_csv(options)
         send_data data, type: Mime::CSV, disposition: "attachment; filename=#{filename}.csv"
       end
