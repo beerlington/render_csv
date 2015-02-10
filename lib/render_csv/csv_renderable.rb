9380ae4c139efb8076ba1d74599b1e07576f7cef
aphilippi-testfeed <andreas.philippi@testfeed.co.uk>
2015-02-10 10:28:05 +0200

    enable custom column headers

--- lib/render_csv/csv_renderable.rb
+++ lib/render_csv/csv_renderable.rb
@@ -15,6 +15,7 @@ module RenderCsv
       columns &= options[:only].map(&:to_s) if options[:only]
       columns -= options[:except].map(&:to_s) if options[:except]
       columns += options[:add_methods].map(&:to_s) if options[:add_methods]
+      columns.collect! { |c| options[:headers][c] || c } if options[:headers]
 
       CSV.generate(encoding: 'utf-8') do |row|
         row << columns
